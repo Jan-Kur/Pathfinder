291b0857f0f0d8cd30a1c72b662e7d32993f274b
Jan Kur <jankur.business@gmail.com>
2025-02-02 23:39:12 +0100

    added github workflow

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: '/Pathfinder'
-		}
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: '/Pathfinder'
+        }
+    }
 };
 
 export default config;
